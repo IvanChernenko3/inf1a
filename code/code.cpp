3504046f6cd4e6cc09ee22d2cb8b747c59b83d1c
IvanChernenko3 <99959203+IvanChernenko3@users.noreply.github.com>
2022-03-22 21:22:50 +0100

    Update code.cpp

--- code/code.cpp
+++ code/code.cpp
@@ -1,22 +1,27 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
 	int n;
 	int lp = 0;
-	int d = 0;
+	int dzielniki = 0;
 	int p = 2;
 
-	bool flaga = true;
 	cout << "Ile liczb wygenerowac: ";
 	cin >> n;
 
-	while (n < lp) {
-		for (int d = 2; d <= p-1; d++) {
+	while (lp < n) {
+		for (int d = 2; d <= p - 1; d++) {
 			if (p % d == 0) {
-				flaga = false; //false - nie jest liczbą parzystą
+				dzielniki++;
 			}
 		}
+		if (dzielniki == 0) {
+			cout << p;
+			lp++;
+		}
+		p++;
+		dzielniki = 0;
 	}
 }
