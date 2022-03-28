cceb4d91b03c3460ab5e4459ae1446edaff733cb
IvanChernenko3 <ivan.chernenko061@gmail.com>
2022-03-28 10:04:51 +0200

    liczby pierwsze, NWD i NWW

--- code/code.cpp
+++ code/code.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main()
 {
-	int n;
+	//kod na wilyczanie liczb pierwszych
+	/* int n;
 	int lp = 0;
 	int dzielniki = 0;
 	int p = 2;
@@ -18,10 +19,57 @@ int main()
 			}
 		}
 		if (dzielniki == 0) {
+<<<<<<< HEAD
+			cout << p << endl;
+=======
 			cout << p;
+>>>>>>> af345649dd9cc7d5e8905f38de207df410013fda
 			lp++;
 		}
 		p++;
 		dzielniki = 0;
+<<<<<<< HEAD
+	}*/ 
+
+	//kod na obliczanie NWD
+	/*int a, b;
+	cout << "Pierwsza liczba: ";
+	cin >> a;
+	cout << "Druga liczba: ";
+	cin >> b;
+	
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+	}
+	cout << "Najwiekzsy wspolny dzielnik: " << a;*/
+
+	//kod na obliczanie NWW
+	/*int a, b;
+	int nww;
+	cout << "Pierwsza liczba: ";
+	cin >> a;
+	cout << "Druga liczba: ";
+	cin >> b;
+	int iloczyn = a * b;
+	
+
+	while (a != b) {
+		if (a < b) {
+			b = b - a;
+		}
+		else {
+			a = a - b;
+		}
+		nww = iloczyn / a;
+	}
+	cout << "Najwieksza wspolna wielokrotnosc: " << nww; */
+}
+=======
 	}
 }
+>>>>>>> af345649dd9cc7d5e8905f38de207df410013fda
