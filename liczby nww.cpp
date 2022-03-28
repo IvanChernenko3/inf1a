62440fe78879ada3962ebf0856f41a323b45ca11
Bartosz Wójcik (Uczeń) <bwojcik472@technikumkreatywne.pl>
2022-03-28 09:42:10 +0200

    liczby nww

---
+++ liczby nww.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Pierwsza liczba:";
+    cin >> a; 
+    cout << "Druga liczba:";
+    cin >> b;
+
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+        cout << "Najwiekszy wspólny dzielnik:" << a;
+    }
+}
+
