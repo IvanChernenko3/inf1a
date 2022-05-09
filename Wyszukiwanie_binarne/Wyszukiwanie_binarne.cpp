e9e984f4fe43fe635afd76f58d0ffa10b6005b6c
IvanChernenko3 <ivan.chernenko061@gmail.com>
2022-05-09 09:44:25 +0200

    wyszukiwanie binarne

--- Wyszukiwanie_binarne/Wyszukiwanie_binarne.cpp
+++ Wyszukiwanie_binarne/Wyszukiwanie_binarne.cpp
@@ -1,7 +1,43 @@
 ﻿#include <iostream>
 using namespace std;
 
+int funkcja(int tab[], int szukana);
+
+
 int main()
 {
-    cout << "Hello World!\n";
+    int szukana;
+    int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+    int pozycja;
+
+    cout << "Podaj liczbe ktora chcesz znalezc: \n";
+    cin >> szukana;
+    if (funkcja(tab, szukana) == -1) {
+        cout << "Nie ma liczby.\n";
+    }
+    else {
+        cout << "Liczbą którą szukasz znajduje się na pozycji " << funkcja(tab, szukana);
+    }
+}
+
+int funkcja(int tab[], int szukana) {
+    int l, p, sr;
+
+    l = 0;
+    p = 15;
+    sr = (l + p) / 2;
+    while (l <= p) {
+        if (tab[sr] == szukana) {
+            return sr;
+        }
+        if (tab[sr] > szukana) {
+            p = sr - 1;
+        }
+        else {
+            l = sr + 1;
+        }
+        sr = (l + p) / 2;
+    }
+
+    return -1;
 }
