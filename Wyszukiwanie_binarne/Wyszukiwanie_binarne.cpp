cfe9576695856de05c965abb31aab5026a96da63
IvanChernenko3 <99959203+IvanChernenko3@users.noreply.github.com>
2022-05-16 09:40:58 +0200

    Update Wyszukiwanie_binarne.cpp

--- Wyszukiwanie_binarne/Wyszukiwanie_binarne.cpp
+++ Wyszukiwanie_binarne/Wyszukiwanie_binarne.cpp
@@ -8,7 +8,6 @@ int main()
 {
     int szukana;
     int tab[15] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
-    int pozycja;
 
     cout << "Podaj liczbe ktora chcesz znalezc: \n";
     cin >> szukana;
