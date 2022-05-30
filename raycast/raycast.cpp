dfe33e24fd4b819f1cfd8ef309c9c4fb2cb632ee
IvanChernenko3 <ivan.chernenko061@gmail.com>
2022-05-30 10:10:44 +0200

    ray

---
+++ raycast/raycast.cpp
@@ -0,0 +1,24 @@
+﻿#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(600,600), "Shape!");
+    sf::CircleShape shape(300, 5);
+    shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
