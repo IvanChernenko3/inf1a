f6d4b4f96c60090c0491b04b4f5d720d4ef271ab
IvanChernenko3 <ivan.chernenko061@gmail.com>
2022-06-13 10:03:32 +0200

    game

---
+++ Dungeon/Dungeon.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+	system("Color C"); //text color
+	cout << "THE DUNGEON GAME\n";
+
+	int userChoice;
+
+
+	cout << "MENU:\n 1. Enter The Dungeon\n 2. Inventory\n 3. Upgrades\n 4. Shop\n 5. Exit\n";
+	cin >> userChoice;
+	
+	switch (userChoice)
+	{
+	case 1:
+		cout << "Choosed 1";
+		break;
+	case 2:
+		cout << "Choosed 2";
+		break;
+	case 3:
+		cout << "Choosed 3";
+		break;
+	case 4:
+		cout << "Choosed 4";
+		break;	
+	case 5:
+		cout << "Choosed 5";
+		break;
+	}
+}
--- raycast/raycast.cpp
+++ raycast/raycast.cpp
@@ -2,9 +2,9 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600,600), "Shape!");
-    sf::CircleShape shape(300, 5);
-    shape.setFillColor(sf::Color::Red);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
     while (window.isOpen())
     {
@@ -22,3 +22,77 @@ int main()
 
     return 0;
 }
+
+
+/*
+#include <SFML/Graphics.hpp>
+#include <iostream>
+void DrawGrid(sf::VertexArray l1, int sizeX, int sizeY, int step);
+sf::RenderWindow window(sf::VideoMode(640, 640), "Shape!");
+int main()
+{
+    
+    sf::CircleShape shape(100, 3);
+    shape.setFillColor(sf::Color::Magenta);
+    sf::Vector2i position = { 0,0 };
+    sf::VertexArray line(sf::LinesStrip, 2);
+    float speed = 1;
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            
+            switch (event.key.code)
+            {
+                case sf::Keyboard::Left:
+                    position.x += -1;
+                    break;
+                case sf::Keyboard::Right:
+                    position.x += 1;
+                    break;
+                case sf::Keyboard::Up:
+                    position.y += -1;
+                    break;
+                case sf::Keyboard::Down:
+                    position.y += 1;
+                    break;
+                //default: position = { 0,0 }; break;
+            }
+
+                
+                
+        }
+
+        window.clear();
+        shape.setPosition(position.x * speed, position.y * speed);
+        
+        DrawGrid(line, 640, 640, 50);
+        window.draw(shape);
+        
+        
+        window.display();
+    }
+
+    return 0;
+}
+
+void DrawGrid(sf::VertexArray l1, int sizeX, int sizeY, int step)
+{
+    for (float x = 0; x < sizeX; x += step)
+    {
+        l1[0].position = sf::Vector2f(0, x);
+        l1[1].position = sf::Vector2f(640, x);
+        window.draw(l1);
+    }
+    for (float x = 0; x < sizeX; x += step)
+    {
+        l1[0].position = sf::Vector2f(x, 0);
+        l1[1].position = sf::Vector2f(x, 640);
+        window.draw(l1);
+    }
+}
+*/
